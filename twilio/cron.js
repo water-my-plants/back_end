6cb453e07f32c6d1e2eeb9ea6d6603c3b4288219
Justin <jdlow2@gmail.com>
2019-02-06 19:18:42 -0500

    Adds authentication middleware for plants

--- twilio/cron.js
+++ twilio/cron.js
@@ -2,18 +2,6 @@ const CronJob = require('cron').CronJob;
 const moment = require('moment');
 const { sendMessage } = require('./send_sms');
 
-/* two ways to do this:
-  1) when a watering schedule is created, schedule a job for each watering time
-  2) check the server every minute to see if there is a watering in the next hour.
-    if there is, send a notification.
-  
-  The first approach seems simpler to me right now. So we need to add:
-  1) when a watering time is added, schedule a reminder if notifications is TRUE
-  2) when a watering time is deleted, deleted the reminder
-
-  We'll need to reformat the date, either here (probably) or before the function is called.
-*/
-
 /* takes in an object shaped like so: {
   username: 'justin',
   plant: 'bonsai',
@@ -23,13 +11,7 @@ const { sendMessage } = require('./send_sms');
   */
 module.exports = {
   notifier: notification => {
-    console.log('nofier', notification);
-
-    // will also need a phone number to pass to send_sms
-    // function takes in a date and schedules a new Cronjob
-    const reminderTime = notification.watering_time; // redundant? do i need to convert to a date?
-    console.log(`reminderTime is ${reminderTime}`);
-
+    const reminderTime = notification.watering_time;
     // ensure date is not in the past
     if (reminderTime >= new Date()) {
       const notify = new CronJob(
